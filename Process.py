252720439b943ecfd2deeb666549936ddbd936bf
schessw <42581659+schessw@users.noreply.github.com>
2018-09-28 22:28:01 +0800

    not needed

--- Process.py
+++
@@ -1,7 +0,0 @@
-class Process():
-    def __init__(self, processLabel, arrivalTime, burstTime, priority, remainingTime):
-        self.processLabel = processLabel
-        self.arrivalTime = arrivalTime
-        self.burstTime = burstTime
-        self.priority = priority
-        self.remainingTime = remainingTime
